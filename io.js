86dd6a374ffc9ebf7c1b5471e3c45566099837a9
gosho-kazuya <gosho-kazuya@919.jp>
2017-04-01 16:46:06 +0900

    fix fs

--- io.js
+++ io.js
@@ -1,12 +1,11 @@
 var ipc = require('electron').ipcRenderer
 var authButton = document.getElementById('auth-button')
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var fs = require('fs');
 
 /*
  * input stdin and set data to html
  */
 function input() {
-  var fs = require('fs');
   fs.readFile('/tmp/a', 'utf8', function (err, data) {
     if (err) return console.log(err)
 
